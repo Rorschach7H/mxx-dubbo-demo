a1feb55659aac3681924dcf7ee0f7cde0bdda588
meixiaoxi <meixiaoxi110@gmail.com>
2018-09-19 18:07:53 +0800

    初始化项目。。。

---
+++ src/main/java/com/meixiaoxi/demo/dubbo/MxxDubboDemoApplication.java
@@ -0,0 +1,12 @@
+package com.meixiaoxi.demo.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxxDubboDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxxDubboDemoApplication.class, args);
+    }
+}
